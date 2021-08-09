9fc2b9dd372410762a63305e28282be433f16ecb
Felipe Merchan <felipe-sc@hotmail.es>
2021-08-08 22:39:07 -0500

    feat: finishing detailing the player

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,16 +1,30 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable import/no-unresolved */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
 
 import '@styles/pages/Player.scss';
 
 const Player = (props) => {
   const { id } = props.match.params;
+  const [loading, setLoading] = useState(true);
+  const hasPlaying = Object.keys(props.playing).length > 0;
 
-  return (
+  useEffect(() => {
+    props.getVideoSource(id);
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <h2>Cargando...</h2>;
+  }
+
+  return hasPlaying ? (
     <div className='Player'>
       <video controls autoPlay>
-        <source src='' type='video/mp4' />
+        <source src={props.playing.source} type='video/mp4' />
       </video>
       <div className='Player-back'>
         <button type='button' onClick={() => props.history.goBack()}>
@@ -18,7 +32,19 @@ const Player = (props) => {
         </button>
       </div>
     </div>
+  ) : (
+    <Redirect to='/404/' />
   );
 };
 
-export default Player;
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+
+const mapDispatchToProps = {
+  getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
