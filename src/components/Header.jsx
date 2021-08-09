e34c1b14397d8e0729334faf4268277c0ed0f526
Felipe Merchan <felipe-sc@hotmail.es>
2021-08-08 21:25:59 -0500

    feat: add validation for login and logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 
 import platziVideoLogo from '../assets/static/images/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/images/icons/user.svg';
@@ -12,6 +13,10 @@ import '@styles/components/Header.scss';
 const Header = (props) => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
 
   return (
     <header className='Header'>
@@ -34,14 +39,20 @@ const Header = (props) => {
           <span>Mi perfil</span>
         </div>
         <ul>
-          <li>
-            <a href='/'>Cuenta</a>
-          </li>
-          <li>
-            <Link to='/login'>
-              Iniciar sesión
-            </Link>
-          </li>
+          {hasUser ?
+            <li>
+              <a href='/'>{user.email}</a>
+            </li> :
+            null}
+          {hasUser ?
+            <li>
+              <a href='#logout' onClick={handleLogout}>Cerrar Sesión</a>
+            </li> :
+            <li>
+              <Link to='/login'>
+                Iniciar sesión
+              </Link>
+            </li>}
         </ul>
       </div>
     </header>
@@ -54,4 +65,8 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
