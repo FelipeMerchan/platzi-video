8a92f38670ce98f1a45f56b195c501df8624f5bd
Felipe Merchan <felipe-sc@hotmail.es>
2021-08-08 20:57:11 -0500

    feat: add login form with Redux
    Connect form information with Redux

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-unresolved */
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loginRequest } from '../actions';
 
 import '@styles/pages/Login.scss';
 import googleIcon from '../assets/static/images/google.svg';
 import twitterIcon from '../assets/static/images/twitter.svg';
 
-const Login = () => {
+const Login = (props) => {
   const [form, setValues] = useState({
     email: '',
   });
@@ -20,7 +22,8 @@ const Login = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(form);
+    props.loginRequest(form);
+    props.history.push('/');
   };
 
   return (
@@ -78,4 +81,8 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapDispatchToProps = {
+  loginRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
